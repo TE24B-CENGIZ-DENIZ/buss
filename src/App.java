6250e95ad066e80b4cea30d57b4a0bb476594a00
TE24B-CENGIZ-DENIZ <deniz.cengiz@elev.ga.ntig.se>
2026-09-18 10:23:25 +0200

    a

--- src/App.java
+++ src/App.java
@@ -9,26 +9,41 @@ void main() {
         int places_left = 20;
         float price = 29.990f;
         int date_of_birth = 0;
-        if (answer == "yes") {
+        if (answer.equals("yes")) {
 
-            System.out.println("ok here you go sit");
+            System.out.println("where would you like to book(numb from 0-19)");
+            String booking = IO.readln("choose your place: ");
+            int booked_place_num = Integer.parseInt(booking);
+            try {
+                booked_place_num = Integer.parseInt(booking);
+            } catch (NumberFormatException g) {
+                IO.println("not a place in this bus ");
+                return;
+            }
+
             for (int i = 0; i < busplaces.length; i++) {
+                // kolla att index inte är bokat
+                if (booked_place_num == i) {
+                    IO.println("the place is booked sorry");
+                    return;
+                }
+
                 try {
                     date_of_birth = Integer.parseInt(IO.readln("your date of birth?: "));
+                    busplaces[i] = " " + date_of_birth; // lägg in bokning i array bussplaces
+
                 } catch (NumberFormatException e) {
                     IO.println("where you even born ? try on the next bus im bored");
-
-                    return;
-
+                    --i;
+                    break;
                 }
-                busplaces[i] = " "+ date_of_birth;
-                --places_left; 
-                IO.println( busplaces[i]);
-            }
-           
-          
 
-        } else if (answer == "no") {
+                --places_left;
+                // IO.println(busplaces[i]);
+
+            }
+
+        } else if (answer.equals("no")) {
 
             System.out.println("bye");
             continue;
@@ -38,6 +53,17 @@ void main() {
 
         }
         IO.println("total earnings so far =  " + (20 - places_left) * price + " $ ");
+        IO.println("you have " + (20 - places_left) + " passanger(s) ");
+
+        // for(String places: busplaces){
+        // IO.println(places);
+
+        // }
+        // : means for each item
+        for (int i = 0; i < busplaces.length; i++) {
+            IO.println(busplaces[i]);
+
+        }
     }
 
 }
